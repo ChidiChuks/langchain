2d6591e9d2b97a827c0523cce563d8951b4fe0db
Chidiebere Chukwuma <chukschiboy@gmail.com>
2023-08-10 19:30:48 +0100

    Update output_parser.py
    revert

--- libs/langchain/langchain/agents/chat/output_parser.py
+++ libs/langchain/langchain/agents/chat/output_parser.py
@@ -1,36 +1,29 @@
 import json
 import re
 from typing import Union
+
 from langchain.agents.agent import AgentOutputParser
 from langchain.agents.chat.prompt import FORMAT_INSTRUCTIONS
 from langchain.schema import AgentAction, AgentFinish, OutputParserException
 
 FINAL_ANSWER_ACTION = "Final Answer:"
 
+
 class ChatOutputParser(AgentOutputParser):
-    """
-    Output parser for the chat agent.
-    """
+    """Output parser for the chat agent."""
 
     pattern = re.compile(r"^.*?`{3}(?:json)?\n(.*?)`{3}.*?$", re.DOTALL)
-    """
-    Regex pattern to parse the output.
-    """
+    """Regex pattern to parse the output."""
 
     def get_format_instructions(self) -> str:
-        """
-        Returns the format instructions.
-        """
         return FORMAT_INSTRUCTIONS
 
     def parse(self, text: str) -> Union[AgentAction, AgentFinish]:
-        """
-        Parses the text and returns AgentAction or AgentFinish.
-        """
         includes_answer = FINAL_ANSWER_ACTION in text
         try:
             found = self.pattern.search(text)
             if not found:
+                # Fast fail to parse Final Answer.
                 raise ValueError("action not found")
             action = found.group(1)
             response = json.loads(action.strip())
@@ -43,6 +36,7 @@ class ChatOutputParser(AgentOutputParser):
             return AgentAction(
                 response["action"], response.get("action_input", {}), text
             )
+
         except Exception:
             if not includes_answer:
                 raise OutputParserException(f"Could not parse LLM output: {text}")
